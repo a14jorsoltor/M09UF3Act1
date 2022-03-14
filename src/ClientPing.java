f5630714b3369d158abcfb65845cdeb6ea3a30bd
Jordi <a14jorsoltor@inspedralbes.cat>
2022-03-14 13:51:32 +0100

    Initial commit

---
+++ src/ClientPing.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientPing {
+    public static void main(String[] args) throws IOException {
+        String hostName = "192.168.205.113";
+        int portNumber = 8005;
+        if (args.length != 2) {
+            System.err.println("Usage: java EchoClient " + hostName + " " + portNumber);
+
+        }
+
+        try (
+                Socket echoSocket = new Socket(hostName, portNumber);
+                PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(echoSocket.getInputStream()));
+                BufferedReader stdIn = new BufferedReader(
+                        new InputStreamReader(System.in))
+        ) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("Pong dice: " + in.readLine());
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                    hostName);
+
+        }
+    }
+}
+
